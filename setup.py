6806bbe5b89ee6025ac5ca22220cd3dc62ce1dad
Julian Vossen <julian@cosmos.video>
2021-06-03 22:27:37 +0100

    Update long description for PyPI

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-  name = 'reloading',         
-  packages = ['reloading'],   
-  version = '1.1.0',      
+  name='reloading',         
+  packages=['reloading'],   
+  version='1.1.1',      
   license='MIT',        
-  description = 'Reloads source code of a running program without losing state',
-  author = 'Julian Vossen',
-  author_email = 'pypi@julianvossen.de',
-  url = 'https://github.com/julvo/reloading',
-  download_url = 'https://github.com/julvo/reloading/archive/v1.1.0.tar.gz',
-  keywords = ['reload', 'reloading', 'refresh', 'loop', 'decorator'],
+  description='Reloads source code of a running program without losing state',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author='Julian Vossen',
+  author_email='pypi@julianvossen.de',
+  url='https://github.com/julvo/reloading',
+  download_url='https://github.com/julvo/reloading/archive/v1.1.1.tar.gz',
+  keywords=['reload', 'reloading', 'refresh', 'loop', 'decorator'],
   install_requires=[],
   classifiers=[
     'Development Status :: 3 - Alpha',
